6632c1e4e578af08d94b1a18a6c0b1ef4ce80f53
bbierman <bbierman7@gmail.com>
2019-05-01 14:51:39 -0500

    Updates

---
+++ precache-manifest.6b6981165e3fb98bde5ed2a69d8d978d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efa38169e370d26d1ed84af45a61eeef",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "652db285340d5cbeacac",
+    "url": "/clicky-game/static/css/main.d98c6a3d.chunk.css"
+  },
+  {
+    "revision": "c65f0670c7d7a243f52e",
+    "url": "/clicky-game/static/js/2.f75e8bbc.chunk.js"
+  },
+  {
+    "revision": "652db285340d5cbeacac",
+    "url": "/clicky-game/static/js/main.1fc28a58.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
